e0a1541b982dd19d571bd3eeaf829695e37e49bb
Rijumone Choudhuri <rijumone@pop-os.localdomain>
2020-07-19 03:57:36 +0530

    changed port to 7001

--- server.py
+++ server.py
@@ -52,6 +52,6 @@ def upload_file():
 if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
-        port='5000',
+        port='7001',
         debug=True if os.getenv('FLASK_PYOCR_DEBUG') == 'true' else False,
         )
